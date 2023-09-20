c7194ea7d5d56f0cc2bd2eb2aa1fc948db559548
nikkhil31 <nikhilimbad@gmail.com>
2023-09-20 18:48:17 +0530

    Add Favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,9 +13,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.ico",
+        src: "/favicon/favicon.ico",
         sizes: "any",
         type: "image/x-icon",
+      },
+      {
+        src: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   };
